346e05f2b2522f26e61ba730d46e4acbf0675d12
Thouis (Ray) Jones <thouis@gmail.com>
2016-07-28 15:16:14 -0400

    error and deriv computation

--- train.py
+++ train.py
@@ -1,10 +1,15 @@
 import sys
 import os
 import glob
+import time
 
 import h5py
 import imread
 import skimage.measure
+
+import numpy as np
+
+import malis as m
 
 from keras.layers.core import Layer, Activation, Dropout
 from keras.layers.convolutional import Convolution2D, MaxPooling2D, ZeroPadding2D, AveragePooling2D
@@ -12,9 +17,6 @@ from keras.layers.normalization import BatchNormalization
 from keras.layers import Input, merge
 from keras.models import Model, Sequential
 from keras import backend as K
-from keras.utils.visualize_util import plot
-
-import numpy as np
 
 def residual_block(input, num_feature_maps, filter_size=3):
     conv_1 = BatchNormalization(axis=1, mode=2)(input)
@@ -58,6 +60,55 @@ def load_label_volume(label_directory, num_labels, h5):
     labels[...] = relabel
     return labels
 
+
+def update_predictions(images, affinities, model):
+    num_images = images.shape[0]
+    st = time.time()
+    for idx in range(num_images):
+        input_volume = np.stack([images[min(max(idx + offset, 0), num_images - 1), ...]
+                                 for offset in range(-2, 3)],
+                                axis=0)
+        pred = model.predict_on_batch(input_volume[np.newaxis, ...])
+        affinities[:, idx, ...] = pred
+    print("Prediction took {} seconds".format(int(time.time() - st)))
+
+
+def compute_malis_counts(affinities, labels):
+    nhood = m.mknhood3d(radius=1)
+    assert nhood.shape[0] == affinities.shape[0]
+    subvolume_shape = labels.shape
+    node_idx_1, node_idx_2 = m.nodelist_like(subvolume_shape, nhood)
+    node_idx_1, node_idx_2 = node_idx_1.ravel(), node_idx_2.ravel()
+    print (node_idx_1[:10], node_idx_2[:10], labels.shape)
+    flat_labels = labels[...].ravel()
+    flat_affinties = affinities[...].ravel()
+    pos_counts = m.malis_loss_weights(flat_labels,
+                                      node_idx_1, node_idx_2,
+                                      flat_affinties,
+                                      1)
+    neg_counts = m.malis_loss_weights(flat_labels,
+                                      node_idx_1, node_idx_2,
+                                      flat_affinties,
+                                      0)
+    pos_counts = pos_counts.reshape(affinities.shape)
+    neg_counts = neg_counts.reshape(affinities.shape)
+    return pos_counts, neg_counts
+
+def err_and_deriv(affinities, pos_counts, neg_counts, idx=74118595):
+    V_Rand_split = (affinities ** 2) * pos_counts / pos_counts.sum()
+    V_Rand_merge = ((1.0 - affinities) ** 2) * neg_counts / neg_counts.sum()
+    sum_VRS = V_Rand_split.sum()
+    sum_VRM = V_Rand_merge.sum()
+    print (sum_VRS, sum_VRM, V_Rand_split.ravel()[idx], V_Rand_merge.ravel()[idx])
+    err = 2 * sum_VRS * sum_VRM / (sum_VRS + sum_VRM)
+    d_VRS_d_aff = 2 * affinities * pos_counts / pos_counts.sum()
+    d_VRM_d_aff = (2 * affinities - 2) * neg_counts / neg_counts.sum()
+    d_err_d_aff = 2 * ((d_VRS_d_aff * sum_VRM  ** 2 + d_VRM_d_aff * sum_VRS ** 2) / 
+                       (sum_VRS + sum_VRM) ** 2)
+    print (d_VRS_d_aff.ravel()[idx], d_VRM_d_aff.ravel()[idx], d_err_d_aff.ravel()[idx])
+    return err, d_err_d_aff
+
+
 if __name__ == '__main__':
     h5 = h5py.File('input_labels_output.hdf5')
     images = load_image_volume(sys.argv[1], h5)
@@ -78,13 +129,9 @@ if __name__ == '__main__':
 
     model = Model(input=x, output=output)
 
-    affinities = h5.require_dataset('affinities', (num_images, 3) + images.shape[1:], dtype=np.float32)
+    affinities = h5.require_dataset('affinities', (3,) + images.shape, dtype=np.float32)
 
-    for idx in range(num_images):
-        print("pred", idx)
-        input_volume = np.stack([images[min(max(idx + offset, 0), num_images - 1), ...]
-                                 for offset in range(-2, 3)],
-                                axis=0)
-        pred = model.predict_on_batch(input_volume[np.newaxis, ...])
-        print(pred.shape)
-        affinities[idx, ...] = pred
+    update_predictions(images, affinities, model)
+    pos_counts, neg_counts = compute_malis_counts(affinities, labels)
+
+    err, d_err_d_aff = err_and_deriv(affinities[...], pos_counts, neg_counts)
